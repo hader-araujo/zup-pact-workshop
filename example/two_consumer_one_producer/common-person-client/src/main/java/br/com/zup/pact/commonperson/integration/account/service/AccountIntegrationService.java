421549f17dd11fd5f9b461a70628dd6b767823d8
Hader <hader.araujo@gmail.com>
2020-06-29 10:19:48 -0300

    example two consumers and one producer

---
+++ example/two_consumer_one_producer/common-person-client/src/main/java/br/com/zup/pact/commonperson/integration/account/service/AccountIntegrationService.java
@@ -0,0 +1,9 @@
+package br.com.zup.pact.commonperson.integration.account.service;
+
+import br.com.zup.pact.commonperson.dto.BalanceDTO;
+
+import java.util.Optional;
+
+public interface AccountIntegrationService {
+    Optional<BalanceDTO> getBalance(Integer accountId);
+}
---
+++ example/two_consumer_one_producer/legal-person-client/src/main/java/br/com/zup/pact/legalperson/integration/account/service/AccountIntegrationService.java
@@ -0,0 +1,9 @@
+package br.com.zup.pact.legalperson.integration.account.service;
+
+import br.com.zup.pact.legalperson.dto.BalanceDTO;
+
+import java.util.Optional;
+
+public interface AccountIntegrationService {
+    Optional<BalanceDTO> getBalance(Integer accountId);
+}
